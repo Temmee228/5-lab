85de3122c06bac26104fcc28367065c92f15e9fc
Temmee228 <74060902+Temmee228@users.noreply.github.com>
2021-01-20 17:49:14 +0300

    e

---
+++ 5 laba/5.js
@@ -0,0 +1,58 @@
+  
+let equalButton = document.getElementById('equal');
+function insert(num) {
+    if (document.form.textview.value.indexOf(' 0') === -1) {
+        document.form.textview.value = document.form.textview.value + num
+    } else {
+        alert('нельзя')
+    }
+}
+function clean() {
+    document.form.textview.value = " ";
+}
+equalButton.onclick = function() {
+    let exp = document.form.textview.value;
+    if(document.form.textview.value.indexOf('/0') !== -1){
+        alert("бесконечность")
+    }
+    else {
+        document.form.textview.value = document.form.textview.value = ` ${eval(exp)}`;
+    }
+};
+function checkOperations(num){
+    if ( (document.form.textview.value.indexOf('1') !== -1          
+        || document.form.textview.value.indexOf('2') !== -1            
+        || document.form.textview.value.indexOf('3') !== -1
+        || document.form.textview.value.indexOf('4') !== -1
+        || document.form.textview.value.indexOf('5') !== -1
+        || document.form.textview.value.indexOf('6') !== -1
+        || document.form.textview.value.indexOf('7') !== -1
+        || document.form.textview.value.indexOf('8') !== -1
+        || document.form.textview.value.indexOf('9') !== -1) &&
+        (document.form.textview.value.indexOf('-') === -1
+            && document.form.textview.value.indexOf('+') === -1
+            && document.form.textview.value.indexOf('*') === -1
+            && document.form.textview.value.indexOf('/') === -1)
+    )
+    {
+        insert(num);
+    } else {
+        alert('нельзя');
+    }
+}
+function checkZero(num){
+    if ( document.form.textview.value.indexOf('1') !== -1                        
+        || document.form.textview.value.indexOf('2') !== -1
+        || document.form.textview.value.indexOf('3') !== -1
+        || document.form.textview.value.indexOf('4') !== -1
+        || document.form.textview.value.indexOf('5') !== -1
+        || document.form.textview.value.indexOf('6') !== -1
+        || document.form.textview.value.indexOf('7') !== -1
+        || document.form.textview.value.indexOf('8') !== -1
+        || document.form.textview.value.indexOf('9') !== -1)
+    {
+        insert(num);
+    } else {
+        alert('нельзя');
+    }
+}
